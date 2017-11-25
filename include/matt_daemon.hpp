a8e9844658cba00f5c34e385911dedf240cee3c6
IndieDriver <amathias63@gmail.com>
2017-11-25 19:26:50 +0100

    daemon setup

--- include/matt_daemon.hpp
+++ include/matt_daemon.hpp
@@ -1,3 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <signal.h>
--- src/matt_daemon.cpp
+++ src/matt_daemon.cpp
@@ -1,8 +1,64 @@
 #include "matt_daemon.hpp"
 
-int main(int argc, char **argv)
+static void skeleton_daemon()
 {
-	(void)argc;
-	(void)argv;
-	return 0;
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    /* Fork off for the second time*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* Set new file permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close (x);
+    }
+
+    /* Open the log file */
+    /* openlog ("firstdaemon", LOG_PID, LOG_DAEMON); */
+}
+
+int main(void) {
+	if (getuid() != 0) {
+		std::cerr << "Error: must be root" << std::endl;
+		exit(1);
+	}
+	skeleton_daemon();
+	sleep(20);
+	return (0);
 }
