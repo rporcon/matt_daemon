51a455f3b7ab7e9b866268fd601302d69d6b9567
akio <rporcon@student.42.fr>
2017-11-26 16:18:51 +0100

    add lock + init sig/fd

--- include/matt_daemon.hpp
+++ include/matt_daemon.hpp
@@ -14,12 +14,16 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/file.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include <libgen.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <poll.h>
-#include <sys/socket.h>
 #include <netdb.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
@@ -28,7 +32,9 @@
 using namespace std;
 
 # define BUF_SIZE 1024
-# define MAX_CLIENT 3
+# define MAX_SOCK 4
+
+extern int	g_lock_fd;
 
 void		err_exit(const char *err_msg);
 void		perr_exit(const char *str);
